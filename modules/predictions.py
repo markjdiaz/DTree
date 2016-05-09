7ed0a37566b4b2a33820d2fd202ad6c5eb43055d
ktanaka086 <ktanaka086@gmail.com>
2016-05-09 17:46:31 -0500

    Produce predicted results in a csv file
    But this isn't a right implementation, so copy and paste are needed
    before submitting the results

--- modules/predictions.py
+++ modules/predictions.py
@@ -15,3 +15,18 @@ def create_predictions(tree, predict):
     Given a tree and a url to a data_set. Create a csv with a prediction for each result
     using the classify method in node class.
     '''
+    f = predict
+    t = [x for x in parse(f, True)]
+    t.pop()
+    #print t
+    [y for y in t]
+    #print i
+    test_outcome = [z[0] for z in y]
+    tree_outcome = [tree.classify(x) for x in y]
+    #print test_outcome
+    #print tree_outcome
+    create_predictions = open('data\predict.csv','wb')    
+    for i in xrange(len(test_outcome)):
+        create_predictions.write('{}, {}\n'.format(test_outcome[i],tree_outcome[i]))
+    create_predictions.close()
+    return
